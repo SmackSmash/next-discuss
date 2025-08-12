08b2d58aca2dd34efab00b9bf711e5d8098f946f
Dan Moffat <danmoffat@gmail.com>
2025-08-12 04:41:13 +0100

    More auth config

--- src/app/api/auth/callback/google/page.tsx
+++ src/app/api/auth/callback/google/page.tsx
@@ -1,15 +1,3 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-    })
-  ],
-  secret: process.env.NEXTAUTH_SECRET
-};
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export default function AuthCallbackPage() {
+  return <div>What goes here?</div>;
+}
