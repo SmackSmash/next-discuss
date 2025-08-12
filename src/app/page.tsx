53603334614169fc64b107ed1070fe8c0a795df7
Dan Moffat <danmoffat@gmail.com>
2025-08-12 03:28:23 +0100

    Added HeroUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+'use client';
+
+import { Button } from '@heroui/react';
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <Button>Click Me</Button>
+    </div>
+  );
 }
