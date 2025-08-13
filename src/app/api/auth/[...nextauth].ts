9129087e47810c8ae02e08030c154a5fda53a9b9
Dan Moffat <danmoffat@gmail.com>
2025-08-13 01:31:36 +0100

    A little restructuring

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -10,6 +10,5 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     })
-  ],
-  secret: process.env.NEXTAUTH_SECRET
+  ]
 });
