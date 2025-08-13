7590374183e422b949e4945b9847a24dcb5af2c6
Dan Moffat <danmoffat@gmail.com>
2025-08-14 00:37:59 +0100

    Added sign out button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import { Button } from '@heroui/react';
-import { signIn } from '@/actions';
+import { signIn, signOut } from '@/actions';
 
 export default function Home() {
   return (
     <div>
       <form action={signIn}>
         <Button type='submit'>Sign In</Button>
+      </form>
+      <form action={signOut}>
+        <Button type='submit'>Sign Out</Button>
       </form>
     </div>
   );
