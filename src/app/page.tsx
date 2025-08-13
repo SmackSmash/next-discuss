d4e775e909e843ec45d2eba6631a054357ee8311
Dan Moffat <danmoffat@gmail.com>
2025-08-13 04:18:54 +0100

    Back on track

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+'use client';
+
+import { Button } from '@heroui/react';
 import { signIn } from '@/actions';
 
-export default async function Home() {
+export default function Home() {
   return (
     <div>
       <form action={signIn}>
-        <button>Sign In</button>
+        <Button type='submit'>Sign In</Button>
       </form>
     </div>
   );
