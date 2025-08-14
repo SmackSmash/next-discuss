c96ccf01030db2b3c31a636bdb9ec1029651165d
Dan Moffat <danmoffat@gmail.com>
2025-08-14 01:03:19 +0100

    Added session provider and profile component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { SessionProvider } from 'next-auth/react';
 import { Providers } from './providers';
 import './globals.css';
 
@@ -26,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${geistSans.variable} ${geistMono.variable} min-h-screen antialiased`}>
-        <Providers>{children}</Providers>
+        <SessionProvider>
+          <Providers>{children}</Providers>
+        </SessionProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from '@heroui/button';
 import { signIn, signOut } from '@/actions';
 import { auth } from '@/auth';
+import Profile from '@/components/profile';
 
 export default async function Home() {
   const session = await auth();
-
-  console.log(session);
 
   return (
     <div className='flex items-center gap-2 p-2'>
@@ -14,13 +13,14 @@ export default async function Home() {
           <form action={signOut}>
             <Button type='submit'>Sign Out</Button>
           </form>
-          <div>{session.user!.email}</div>
+          <div>{session.user?.email}</div>
         </>
       ) : (
         <form action={signIn}>
           <Button type='submit'>Sign In</Button>
         </form>
       )}
+      <Profile />
     </div>
   );
 }
