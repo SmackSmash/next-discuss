81e0449f2fb1c9189955750243bb7eb23dae67e4
Dan Moffat <danmoffat@gmail.com>
2025-08-14 03:48:42 +0100

    Lovely sessions

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,34 +1,49 @@
-import Image from 'next/image';
-import { Button } from '@heroui/button';
-import { signIn, signOut } from '@/actions';
-import { auth } from '@/auth';
-import Profile from '@/components/profile';
+'use client';
 
-export default async function Header() {
-  const session = await auth();
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+  Avatar
+} from '@heroui/react';
+import { signIn, signOut } from '@/actions';
+
+export default function Header() {
+  const session = useSession();
+
+  console.log(session);
 
   return (
-    <div className='flex items-center gap-2 p-2'>
-      {session ? (
-        <>
-          <form action={signOut}>
-            <Button type='submit'>Sign Out</Button>
+    <Navbar className='mb-6 shadow'>
+      <NavbarBrand>
+        <Link href='/' className='font-bold'>
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify='center'>
+        <NavbarItem>
+          <Input />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify='end'>
+        {session.data ? (
+          <>
+            <form action={signOut}>
+              <Button type='submit'>Sign Out</Button>
+            </form>
+            <Avatar src={session.data?.user?.image!} />
+          </>
+        ) : (
+          <form action={signIn}>
+            <Button type='submit'>Sign In</Button>
           </form>
-          <Image
-            src={session.user?.image!}
-            alt={session.user?.name!}
-            width='40'
-            height='40'
-            className='rounded-full'
-          />
-        </>
-      ) : (
-        <form action={signIn}>
-          <Button type='submit'>Sign In</Button>
-        </form>
-      )}
-      {/* BUG! Profie component does not update on signOut */}
-      <Profile />
-    </div>
+        )}
+      </NavbarContent>
+    </Navbar>
   );
 }
