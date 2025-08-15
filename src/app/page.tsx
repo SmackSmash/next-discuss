2b364e32c8abeff416d7057d59ab0f9d8eaf1e15
Dan Moffat <danmoffat@gmail.com>
2025-08-15 02:07:42 +0100

    Scaffolding home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import TopicCreateForm from '@/components/topics/topic-create-form';
+
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className='grid w-full grid-cols-4 gap-4 p-4'>
+      <div className='col-span-3'>
+        <h1 className='-m-2 text-xl'>Top Posts</h1>
+      </div>
+      <TopicCreateForm />
+    </div>
+  );
 }
