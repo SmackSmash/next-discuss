2b0727cdf936e1e7925c8c4aa6ba36ff5bae4a35
Dan Moffat <danmoffat@gmail.com>
2025-08-15 02:52:35 +0100

    Added zod validationi

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,15 @@
 'use server';
 
+import { z } from 'zod';
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, { message: 'Must be lowercase letters or dashes without spaces' }),
+  description: z.string().min(10)
+});
+
 export async function createTopic(formData: FormData) {
   console.log(formData);
 
