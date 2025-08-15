ffc35f55575355257bf7fe3dcc07905ae3cdabf9
Dan Moffat <danmoffat@gmail.com>
2025-08-15 02:22:43 +0100

    Fleshed out topic submit form

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,27 @@
+import { Input, Textarea } from '@heroui/input';
+import { Button } from '@heroui/button';
+import { Popover, PopoverTrigger, PopoverContent } from '@heroui/popover';
+
 export default function TopicCreateForm() {
-  return <div>Topic create form</div>;
+  return (
+    <Popover placement='left'>
+      <PopoverTrigger>
+        <Button color='primary'>Create a topic</Button>
+      </PopoverTrigger>
+      <PopoverContent className='bg-stone-900'>
+        <form className='flex w-80 flex-col gap-4 p-4'>
+          <h3>Create a Topic</h3>
+          <Input label='Name' labelPlacement='outside' placeholder='Name' />
+          <Textarea
+            label='Description'
+            labelPlacement='outside'
+            placeholder='Describe your topic'
+          />
+          <Button type='submit' color='primary'>
+            Submit
+          </Button>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
 }
