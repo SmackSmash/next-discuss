5205477b475017c99ad21e79055f2dc1466a6967
Dan Moffat <danmoffat@gmail.com>
2025-08-15 04:39:27 +0100

    Lovely error state

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -6,10 +6,7 @@ import { type FormEvent, startTransition, useActionState } from 'react';
 
 export default function TopicCreateForm() {
   const [state, formAction, isPending] = useActionState(createTopic, {
-    errors: {
-      name: [],
-      description: []
-    }
+    errors: {}
   });
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -28,12 +25,21 @@ export default function TopicCreateForm() {
       <PopoverContent className='bg-stone-900'>
         <form onSubmit={handleSubmit} className='flex w-80 flex-col gap-4 p-4'>
           <h3>Create a Topic</h3>
-          <Input name='name' label='Name' labelPlacement='outside' placeholder='Name' />
+          <Input
+            name='name'
+            label='Name'
+            labelPlacement='outside'
+            placeholder='Name'
+            isInvalid={!!state.errors.name}
+            errorMessage={state.errors.name}
+          />
           <Textarea
             name='description'
             label='Description'
             labelPlacement='outside'
             placeholder='Describe your topic'
+            isInvalid={!!state.errors.description}
+            errorMessage={state.errors.description}
           />
           <Button type='submit' color='primary'>
             Submit
