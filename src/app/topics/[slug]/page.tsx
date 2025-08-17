95507f3e48238eb39fc8906645a6972e8bd9d1ac
Dan Moffat <danmoffat@gmail.com>
2025-08-17 01:40:17 +0100

    Get slug from params on topic page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,13 @@
-export default function TopicPage() {
-  return <div>Topic Page</div>;
+type TopicPageProps = {
+  params: Promise<{ slug: string }>;
+};
+
+export default async function TopicPage({ params }: TopicPageProps) {
+  const { slug } = await params;
+
+  return (
+    <div>
+      <h1>{slug}</h1>
+    </div>
+  );
 }
