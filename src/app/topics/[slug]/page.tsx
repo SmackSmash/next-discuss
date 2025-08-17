2ba82bee621360793b5a206448957720c9513f32
Dan Moffat <danmoffat@gmail.com>
2025-08-17 01:48:48 +0100

    Capitalization using lodash

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import * as _ from 'lodash-es';
+
 type TopicPageProps = {
   params: Promise<{ slug: string }>;
 };
@@ -6,8 +8,10 @@ export default async function TopicPage({ params }: TopicPageProps) {
   const { slug } = await params;
 
   return (
-    <div>
-      <h1>{slug}</h1>
+    <div className='grid grid-cols-4 gap-4 p-4'>
+      <div className='col-span-3'>
+        <h1 className='mb-2 text-2xl font-bold'>{_.capitalize(slug)}</h1>
+      </div>
     </div>
   );
 }
