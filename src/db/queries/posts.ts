45f5936a220e3ac40ca2bae3834a35ba7533eae8
Dan Moffat <danmoffat@gmail.com>
2025-08-18 03:37:04 +0100

    This part is extremely rushed

---
+++ src/db/queries/posts.ts
@@ -0,0 +1,19 @@
+import { type Post } from '@prisma/client';
+import { prisma } from '@/db';
+
+type PostWithData = Post & {
+  topic: { slug: string };
+  user: { name: string | null };
+  _count: { comments: number };
+};
+
+export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
+  return prisma.post.findMany({
+    where: { topic: { slug } },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } }
+    }
+  });
+}
