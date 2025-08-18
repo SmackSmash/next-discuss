bb37fcffac23cf6b42c199de276258314022a0a6
Dan Moffat <danmoffat@gmail.com>
2025-08-18 03:48:03 +0100

    Grider has gone off the rails completely!

--- src/components/posts/post-list.tsx
+++ src/components/posts/post-list.tsx
@@ -1,10 +1,16 @@
-import type { Post, User, Topic } from '@prisma/client';
 import Link from 'next/link';
+import { type PostWithData } from '@/db/queries/posts';
 import paths from '@/paths';
 
+type PostListProps = {
+  fetchData: () => Promise<PostWithData[]>;
+};
+
 // TODO: Get list of posts into this component somehow
-export default function PostList() {
-  const renderedPosts = posts.map((post) => {
+export default async function PostList({ fetchData }: PostListProps) {
+  const posts = await fetchData();
+
+  return <div className='space-y-2'>{ posts.map(post => {
     const topicSlug = post.topic.slug;
 
     if (!topicSlug) {
@@ -12,19 +18,15 @@ export default function PostList() {
     }
 
     return (
-      <div key={post.id} className="border rounded p-2">
+      <div key={post.id} className='rounded border p-2'>
         <Link href={paths.postShow(topicSlug, post.id)}>
-          <h3 className="text-lg font-bold">{post.title}</h3>
-          <div className="flex flex-row gap-8">
-            <p className="text-xs text-gray-400">By {post.user.name}</p>
-            <p className="text-xs text-gray-400">
-              {post._count.comments} comments
-            </p>
+          <h3 className='text-lg font-bold'>{post.title}</h3>
+          <div className='flex flex-row gap-8'>
+            <p className='text-xs text-gray-400'>By {post.user.name}</p>
+            <p className='text-xs text-gray-400'>{post._count.comments} comments</p>
           </div>
         </Link>
       </div>
     );
-  });
-
-  return <div className="space-y-2">{renderedPosts}</div>;
+  });}</div>;
 }
