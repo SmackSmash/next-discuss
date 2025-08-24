7c3f089a7b97dd2c477e437a9c6091ce2a955e55
Dan Moffat <danmoffat@gmail.com>
2025-08-24 01:58:39 +0100

    Removed redundant type import

--- src/components/comments/comment-list.tsx
+++ src/components/comments/comment-list.tsx
@@ -1,5 +1,5 @@
 import CommentShow from '@/components/comments/comment-show';
-import { fetchCommentsByPostId, type CommentWithAuthor } from '@/db/queries/comments';
+import { fetchCommentsByPostId } from '@/db/queries/comments';
 
 interface CommentListProps {
   postId: string;
